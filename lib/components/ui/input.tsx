15ab0a2d35e5ca3c5962c87439f981230e45e285
Marshall Kurniawan <shrallvierdo@gmail.com>
2024-08-27 13:49:01 +0700

    feat: add label & tooltip to input

--- lib/components/ui/input.tsx
+++ lib/components/ui/input.tsx
@@ -6,6 +6,7 @@ type InputStyle = "default" | "underline";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
+  container?: { className?: string };
   input?: { className?: string };
   inputStyle?: InputStyle;
   leftNode?: React.ReactNode;
@@ -13,6 +14,9 @@ export interface InputProps
   regex?: "text" | "number";
   errorMessage?: string;
   helperText?: string;
+  alignment?: "vertical" | "horizontal";
+  label?: string;
+  tooltip?: React.ReactNode;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
@@ -20,6 +24,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     {
       className,
       type,
+      container,
       input,
       errorMessage,
       helperText,
@@ -31,6 +36,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       maxLength,
       regex,
       value,
+      label,
+      tooltip,
+      alignment = "vertical",
       ...props
     },
     ref,
@@ -52,59 +60,79 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       }
     }
     return (
-      <div className="lui-flex lui-flex-col lui-gap-y-2">
-        <div
-          className={cn(
-            "lui-flex lui-w-full lui-items-center lui-gap-x-2 lui-text-sm placeholder:lui-text-ocean-dark-10 focus-within:lui-border-ocean-primary-10",
-            inputStyle === "default" &&
-              "lui-rounded-lg lui-bg-ocean-light-20 lui-px-3 lui-py-2 lui-text-ocean-dark-30",
-            inputStyle === "underline" &&
-              "lui-border-b lui-border-ocean-dark-10 lui-pb-1.5 has-[svg]:lui-text-ocean-dark-10",
-            inputStyle === "underline" &&
-              type === "password" &&
-              value &&
-              "lui-text-xl lui-tracking-[10px]",
-            inputStyle === "default" &&
-              disabled &&
-              "lui-bg-ocean-light-20 lui-text-ocean-light-40",
-            inputStyle === "underline" &&
-              disabled &&
-              "lui-border-ocean-light-40 lui-text-ocean-light-40 has-[svg]:lui-text-ocean-light-40",
-            errorMessage && "lui-border-ocean-danger-20",
-            className,
-          )}
-        >
-          {leftNode}
-          <input
-            type={type}
+      <div
+        className={cn(
+          "lui-flex lui-items-start",
+          alignment === "vertical" && "lui-flex-col lui-gap-y-2",
+          alignment === "horizontal" && "lui-flex-row lui-gap-x-6",
+          container && container.className,
+        )}
+      >
+        {label && (
+          <div className="lui-flex lui-w-[156px] lui-max-w-[156px] lui-items-center lui-gap-x-2">
+            <label
+              htmlFor={props.name}
+              className="lui-text-sm lui-font-semibold lui-text-ocean-primary-10"
+            >
+              {label}
+            </label>
+            {tooltip && tooltip}
+          </div>
+        )}
+        <div className="lui-flex lui-flex-col lui-gap-y-2">
+          <div
             className={cn(
-              "lui-w-full lui-outline-none disabled:placeholder:lui-text-ocean-light-40",
+              "lui-flex lui-w-full lui-items-center lui-gap-x-2 lui-text-sm placeholder:lui-text-ocean-dark-10 focus-within:lui-border-ocean-primary-10",
               inputStyle === "default" &&
-                "lui-bg-ocean-light-20 focus:placeholder:lui-text-ocean-light-40",
+                "lui-rounded-lg lui-bg-ocean-light-20 lui-px-3 lui-py-2 lui-text-ocean-dark-30",
               inputStyle === "underline" &&
-                "lui-h-6 lui-text-ocean-dark-30 disabled:lui-bg-white",
-              input && input.className,
-            )}
-            ref={ref}
-            disabled={disabled}
-            {...props}
-            onChange={handleChange}
-            maxLength={Infinity}
-            value={value}
-          />
-          {rightNode}
-        </div>
-        {(errorMessage || helperText) && (
-          <span
-            className={cn(
-              "lui-text-xs lui-text-ocean-dark-10",
-              disabled && "lui-text-ocean-light-40",
-              errorMessage && "lui-text-ocean-danger-20",
+                "lui-border-b lui-border-ocean-dark-10 lui-pb-1.5 has-[svg]:lui-text-ocean-dark-10",
+              inputStyle === "underline" &&
+                type === "password" &&
+                value &&
+                "lui-text-xl lui-tracking-[10px]",
+              inputStyle === "default" &&
+                disabled &&
+                "lui-bg-ocean-light-20 lui-text-ocean-light-40",
+              inputStyle === "underline" &&
+                disabled &&
+                "lui-border-ocean-light-40 lui-text-ocean-light-40 has-[svg]:lui-text-ocean-light-40",
+              errorMessage && "lui-border-ocean-danger-20",
+              className,
             )}
           >
-            {errorMessage ? errorMessage : helperText}
-          </span>
-        )}
+            {leftNode}
+            <input
+              type={type}
+              className={cn(
+                "lui-w-full lui-outline-none disabled:placeholder:lui-text-ocean-light-40",
+                inputStyle === "default" &&
+                  "lui-bg-ocean-light-20 focus:placeholder:lui-text-ocean-light-40",
+                inputStyle === "underline" &&
+                  "lui-h-6 lui-text-ocean-dark-30 disabled:lui-bg-white",
+                input && input.className,
+              )}
+              ref={ref}
+              disabled={disabled}
+              {...props}
+              onChange={handleChange}
+              maxLength={Infinity}
+              value={value}
+            />
+            {rightNode}
+          </div>
+          {(errorMessage || helperText) && (
+            <span
+              className={cn(
+                "lui-text-xs lui-text-ocean-dark-10",
+                disabled && "lui-text-ocean-light-40",
+                errorMessage && "lui-text-ocean-danger-20",
+              )}
+            >
+              {errorMessage ? errorMessage : helperText}
+            </span>
+          )}
+        </div>
       </div>
     );
   },
--- src/pages/Inputs.tsx
+++ src/pages/Inputs.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Icon } from "@/components/ui/icon";
+import { Tooltip } from "@/components/ui/tooltip";
 import { useState } from "react";
 
 function Inputs() {
@@ -103,6 +104,78 @@ function Inputs() {
           value={value}
           onChange={(e) => setValue(e.target.value)}
           disabled
+        />
+      </div>
+      <div className="lui-grid lui-grid-cols-2 lui-items-center lui-gap-2">
+        <Input
+          inputStyle="underline"
+          placeholder="Placeholder"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label="Label"
+        />
+        <Input
+          inputStyle="underline"
+          placeholder="Placeholder"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label="Label w/Tooltip"
+          tooltip={
+            <Tooltip
+              trigger={
+                <Icon
+                  name="question-filled"
+                  className="lui-text-ocean-primary-10"
+                />
+              }
+              side="top"
+              title="Tooltip Title"
+            >
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor.
+              </p>
+            </Tooltip>
+          }
+        />
+      </div>
+      <div className="lui-grid lui-grid-cols-2 lui-items-center lui-gap-2">
+        <Input
+          inputStyle="underline"
+          placeholder="Placeholder"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label="Label"
+          alignment="horizontal"
+        />
+        <Input
+          inputStyle="underline"
+          placeholder="Placeholder"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          label="Label w/Tooltip"
+          alignment="horizontal"
+          tooltip={
+            <Tooltip
+              trigger={
+                <Icon
+                  name="question-filled"
+                  className="lui-text-ocean-primary-10"
+                />
+              }
+              side="top"
+              title="Tooltip Title"
+            >
+              <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+                commodo ligula eget dolor.
+              </p>
+            </Tooltip>
+          }
         />
       </div>
     </div>
