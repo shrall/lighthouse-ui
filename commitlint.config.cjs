7888cc99f8d378160d828d0fd9ac9352d7970f52
Marshall Kurniawan <shrallvierdo@gmail.com>
2024-08-29 14:23:18 +0700

    feat: rename package to lighthouse-ui-react

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "chore", "style", "refactor"]],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "chore", "style", "refactor", "docs"],
+    ],
   },
 };
