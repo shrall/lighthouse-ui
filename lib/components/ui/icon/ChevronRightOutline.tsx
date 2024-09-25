7b4d265ecf255f8a101586705753e1267e401426
Marshall Kurniawan <marshallovierdo@gmail.com>
2024-09-25 09:06:03 +0700

    fix(icon): chevron right outline showing the filled version

--- lib/components/ui/icon/ChevronRightOutline.tsx
+++ lib/components/ui/icon/ChevronRightOutline.tsx
@@ -6,8 +6,11 @@ export const ChevronRightOutline = React.forwardRef<SVGSVGElement, IconProps>(
     return (
       <SVGIcon ref={ref} {...props}>
         <path
-          d="M12 22C17.514 22 22 17.514 22 12C22 6.486 17.514 2 12 2C6.486 2 2 6.486 2 12C2 17.514 6.486 22 12 22ZM9.293 7.707L10.707 6.293L16.414 12L10.707 17.707L9.293 16.293L13.586 12L9.293 7.707Z"
-          fill="currentColor"
+          d="M9 6L15 12L9 18"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </SVGIcon>
     );
