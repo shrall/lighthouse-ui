9eca7539346351c9df4817f0226c437e2d59b011
Marshall Kurniawan <marshallovierdo@gmail.com>
2024-10-25 23:21:19 +0700

    feat(form-input): update to use input component directly

--- src/pages/form/components/FormInput.tsx
+++ src/pages/form/components/FormInput.tsx
@@ -1,28 +1,10 @@
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
-
-type InputStyle = "outline" | "underline";
+import { Input, InputProps } from "@/components/ui/input";
 
 export interface FormInputProps<TFieldValues extends FieldValues>
-  extends React.InputHTMLAttributes<HTMLInputElement> {
-  container?: { className?: string };
-  inputStyle?: InputStyle;
-  leftNode?: React.ReactNode;
-  rightNode?: React.ReactNode;
-  regex?:
-    | {
-        type: "number";
-      }
-    | {
-        type: "custom";
-        pattern: RegExp;
-      };
-  helperText?: string;
-  alignment?: "vertical" | "horizontal";
-  label?: string;
-  tooltip?: React.ReactNode;
+  extends InputProps {
   control: Control<TFieldValues>;
   name: Path<TFieldValues>;
 }
@@ -75,95 +57,22 @@ const FormInput = React.forwardRef(
           const errorMessage = fieldState.error?.message;
 
           return (
-            <div
-              className={cn(
-                "lui-flex lui-w-full lui-items-start",
-                alignment === "vertical" && "lui-flex-col lui-gap-y-2",
-                alignment === "horizontal" && "lui-flex-row lui-gap-x-6",
-                container && container.className,
-              )}
-            >
-              {label && (
-                <div
-                  className={cn(
-                    "lui-flex lui-items-start lui-gap-x-2",
-                    alignment === "horizontal" &&
-                      "lui-min-w-[156px] lui-max-w-[156px]",
-                  )}
-                >
-                  <label
-                    htmlFor={name}
-                    className="lui-text-start lui-text-sm lui-font-semibold lui-text-ocean-primary-10 lui-break-anywhere"
-                  >
-                    {label}
-                  </label>
-                  {tooltip && tooltip}
-                </div>
-              )}
-              <div className="lui-flex lui-w-full lui-flex-col lui-items-start lui-gap-y-1">
-                <div
-                  className={cn(
-                    "lui-flex lui-w-full lui-items-center lui-gap-x-2 lui-text-sm placeholder:lui-text-ocean-dark-10 focus-within:lui-border-ocean-primary-10 has-[svg]:lui-text-ocean-dark-10",
-                    inputStyle === "outline" &&
-                      "lui-rounded-lg lui-bg-ocean-light-20 lui-px-3 lui-py-2",
-                    inputStyle === "underline" &&
-                      "lui-border-b lui-border-ocean-dark-10 lui-pb-1.5",
-                    inputStyle === "underline" &&
-                      type === "password" &&
-                      value &&
-                      "lui-text-xl lui-tracking-[10px]",
-                    inputStyle === "outline" &&
-                      disabled &&
-                      "lui-bg-ocean-light-20 lui-text-ocean-light-40 has-[svg]:lui-text-ocean-light-40",
-                    inputStyle === "underline" &&
-                      disabled &&
-                      "lui-border-ocean-light-40 lui-text-ocean-light-40 has-[svg]:lui-text-ocean-light-40",
-                    errorMessage && "lui-border-ocean-danger-20",
-                  )}
-                >
-                  {leftNode}
-                  <input
-                    id={name}
-                    type={type}
-                    className={cn(
-                      "lui-w-full lui-text-ocean-dark-30 lui-outline-none disabled:placeholder:lui-text-ocean-light-40",
-                      inputStyle === "outline" &&
-                        "lui-bg-ocean-light-20 focus:placeholder:lui-text-ocean-light-40",
-                      inputStyle === "underline" &&
-                        "lui-h-6 disabled:lui-bg-white",
-                      className,
-                    )}
-                    disabled={disabled}
-                    maxLength={maxLength ?? Infinity}
-                    {...props}
-                    {...field}
-                    ref={(e) => {
-                      if (typeof ref === "function") {
-                        ref(e);
-                      } else if (ref) {
-                        ref.current = e;
-                      }
-                      field.ref(e);
-                    }}
-                    onChange={(e) => {
-                      field.onChange(handleChange(e));
-                    }}
-                  />
-                  {rightNode}
-                </div>
-                {(errorMessage || helperText) && (
-                  <span
-                    className={cn(
-                      "lui-text-xs lui-text-ocean-dark-10",
-                      disabled && "lui-text-ocean-light-40",
-                      errorMessage && "lui-text-ocean-danger-20",
-                    )}
-                  >
-                    {errorMessage ? errorMessage : helperText}
-                  </span>
-                )}
-              </div>
-            </div>
+            <Input
+              {...props}
+              {...field}
+              ref={(e) => {
+                if (typeof ref === "function") {
+                  ref(e);
+                } else if (ref) {
+                  ref.current = e;
+                }
+                field.ref(e);
+              }}
+              onChange={(e) => {
+                field.onChange(handleChange(e));
+              }}
+              errorMessage={errorMessage}
+            />
           );
         }}
       />
